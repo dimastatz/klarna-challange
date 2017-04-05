057e6b63c84e2b9f543fc3a1cfeef9fb601cbaf7
Dima Stazenko <dima.stazenko@clicktale.com>
2017-04-05 21:10:40 +0300

    Fix readme file

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -5,9 +5,9 @@ case class Input(inputFile:String, outputFile:String, template: Map[String, Int]
 
 object Boot {
   def main(args: Array[String]): Unit = {
-    val inputFile = Try(args(0)).getOrElse("C:/temp/input.txt")
-    val alphabetFile = Try(args(2)).getOrElse("C:/temp/alphabet.txt")
-    val pw = new PrintWriter(Try(args(1)).getOrElse("C:/temp/output.txt"))
+    val inputFile = Try(args(0)).getOrElse("/temp/input.txt")
+    val alphabetFile = Try(args(2)).getOrElse("/temp/alphabet.txt")
+    val pw = new PrintWriter(Try(args(1)).getOrElse("/temp/output.txt"))
 
     // Load alphabet to map: 0-9 digits
     val alphabet = parseLine(
@@ -32,15 +32,14 @@ object Boot {
 
   def parseLine(lines: Seq[String], template: Map[String, Int]): Array[Int] = {
     val digits = parseLine(lines, 0)
-    val result = digits.map(i => Try(template(i)))
-    result.map(i => i.getOrElse(-1))
+    digits.map(i => Try(template(i)).getOrElse(-1))
   }
 
   def parseLine(lines: Seq[String], index: Int = 0): Array[String] = {
     if(index + 3 > lines.head.length) Array()
     else {
        val key = Range(0, 3).map(i => lines(i).substring(index, index + 3))
-       Array(key.mkString(",")) ++ parseLine(lines, index + 3)
+       Array(key.mkString(",").trim) ++ parseLine(lines, index + 3)
     }
   }
 }
